a1ae96b91626046fe3c2d79eb5de4cbdc14a7a22
Matt Miller and Susan Southall <matt@>
2018-03-19 10:36:09 -0700

    create initial structure, add addition to scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var number1 = parseInt(prompt("Please input a number:"));
+var number2 = parseInt(prompt("Please input another number:"));
+
+var add = function(number1,number2) {
+	return number1 + number2
+};
+
+alert(add(number1,number2));
